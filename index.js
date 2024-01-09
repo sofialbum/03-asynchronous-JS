bd492311432e798dc613cb95beb3f82d8c5fa83a
Sofía González <sofia_1.6@hotmail.com>
2024-01-09 17:24:58 -0300

    finished section

--- index.js
+++ index.js
@@ -19,7 +19,58 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res1Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
+    console.log('Random dog image saved to file');
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  }
+  return '2: READY';
+};
+
+(async () => {
+  try {
+    console.log('1: Will get dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR');
+  }
+})();
+
+/*
+console.log('1: Will get dog pics!');
+getDogPic()
+    .then(x => {
+        console.log(x);
+        console.log('3: Done getting dog pics!');
+    })
+    .catch(err => {
+        console.log('ERROR');
+    });
+*/
+
+/*readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed: ${data}`);
     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
@@ -34,3 +85,4 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch((err) => {
     console.log(err);
   });
+*/
